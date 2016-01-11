fae550a86035e06a28753d8643354ae097151b1d
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2016-01-11 10:10:36 +0100

    Enable suppose-stream & make it a Transform

--- test/suppose-stream.test.js
+++ test/suppose-stream.test.js
@@ -1,94 +1,33 @@
-//not in use yet
-
-return;
-
 var assert = require('assert')
-  , suppose = require('../lib/suppose')
-  //, path = require('path-extra')
-  , fs = require('fs-extra')
-  , P = require('autoresolve')
-  , S = require('string')
-  , readline = require('readline')
-  , Stream = require('stream').Stream
-  , util = require('util')
-  , net = require('net');
+var net    = require('net')
+var path   = require('path')
+var PassThrough = require('stream').PassThrough
+var util   = require('util')
 
-var TEST_DIR = path.join(path.tempdir(), 'test-suppose');
+var fs = require('fs-extra')
+var P = require('autoresolve')
+var S = require('string')
 
+var SupposeStream = require('../lib/suppose-stream')
 
 
-/*function PassthroughStream() {
-  this.writable = true;
-  this.readable = true;
-}
-util.inherits(PassthroughStream, Stream);
+describe('stream', function()
+{
+  it('should respond to a stream', function(done)
+  {
+    var input  = new PassThrough()
+    var output = new PassThrough()
 
-PassthroughStream.prototype.write = function(data) {
-  console.log('derrta')
-  this.emit('data', data);
-};
+    input.pipe(SupposeStream())
+      .when('Hi').respond('Bye')
+    .pipe(output)
 
-PassthroughStream.prototype.end = function() {
-  console.log('END PS')
-  this.emit('end');
-};
+    input.push('Hi')
+    output.once('data', function(chunk, encoding, next)
+    {
+      assert.strictEqual(chunk.toString(), 'Bye')
 
-PassthroughStream.prototype.destroy = function() {
-  this.emit('close');
-};*/
-
-//util.inherits(process.stdout, Stream);
-
-
-describe.skip('+ suppose', function(){
-    beforeEach(function(done){
-        fs.exists(TEST_DIR, function(itDoes){
-            if (itDoes) {
-                fs.remove(TEST_DIR, function(err){
-                    fs.mkdir(TEST_DIR, done);
-                });
-            } else {
-                fs.mkdir(TEST_DIR, done);
-            }
-        });
-    });
-
-    describe('stream', function() {
-        it('should respond to a stream', function(done) {
-            done();
-            return;
-            //var socket = new net.Socket();
-            //socket.connect(path.join(TEST_DIR, 'stream.sock'));
-
-            var server = net.createServer(path.join(TEST_DIR, 'stream.sock'), function(sock) {
-                console.log('HI')
-                var client = net.connect(path.join(TEST_DIR, 'stream.sock'));
-                process.chdir(TEST_DIR);
-                var debugFile = path.join(TEST_DIR, 'debug.txt');
-                suppose(client, sock)
-                  .on('What is your name? ').respond('JP\n')
-                .end(function(){
-                   //nothing happens here since process.stdio doesn't terminate until the process is terminated
-                });
-
-                var rl = readline.createInterface({
-                    input: sock,
-                    output: client
-                });
-
-                rl.question("What is your name? ", function(answer) {
-                    console.log(answer)
-                    assert(answer === 'BOB');
-                    rl.close();
-                    done();
-                });
-            });
-
-            
-
-            
-        });
+      done()
     })
-
-});
-
+  })
+})
