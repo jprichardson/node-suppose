a79a2a5aa087b2c36733fcc3af6211714adfb1d4
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2016-01-11 13:00:29 +0100

    Made SupposeProcess to use internally SupposeStream

--- test/suppose-process.test.js
+++ test/suppose-process.test.js
@@ -1,133 +1,148 @@
+var assert   = require('assert')
+var join     = require('path').join
+var os       = require('os')
+var readline = require('readline')
+
+
+var fs       = require('fs-extra')
+var P        = require('autoresolve')
+var S        = require('string')
 var testutil = require('testutil')
-  , suppose = require('../lib/suppose')
-  , path = require('path')
-  , fs = require('fs-extra')
-  , P = require('autoresolve')
-  , S = require('string')
-  , readline = require('readline')
-  , os = require('os')
 
-var TEST_DIR = ''
+var suppose = require('../lib/suppose')
 
-describe('+ suppose', function(){
-  beforeEach(function(){
-    TEST_DIR = testutil.createTestDir('suppose')
-    fs.writeFileSync(path.join(TEST_DIR, 'README.md'), 'READ IT')
+
+beforeEach(function(done)
+{
+  this.TEST_DIR = testutil.createTestDir('suppose')
+  fs.writeFile(join(this.TEST_DIR, 'README.md'), 'READ IT', done)
+})
+
+describe('process', function()
+{
+  it('should automate the command: npm init', function(done)
+  {
+    var self = this
+
+    process.chdir(this.TEST_DIR)
+
+    suppose('npm', ['init'])
+      .when(/name\: \((\w|\-)+\)\s*/).respond("awesome_package\n")
+      //.when('name: (test-suppose) ').respond('awesome_package\n')
+      .when('version: (1.0.0) ').respond('0.0.1\n')
+      .when('description: ').respond("It's an awesome package man!\n")
+      .when('entry point: (index.js) ').respond("\n")
+      .when('test command: ').respond('npm test\n')
+      .when('git repository: ').respond("\n")
+      .when('keywords: ').respond('awesome, cool\n')
+      .when('author: ').respond('JP Richardson\n')
+      .when('license: (ISC) ').respond('MIT\n')
+      .when('ok? (yes) ' ).respond('yes\n')
+    .on('error', done)
+    .end(function(code)
+    {
+      assert.strictEqual(code, 0)
+
+      var packageObj = require(join(self.TEST_DIR, 'package.json'))
+
+      assert.strictEqual(packageObj.name, 'awesome_package')
+      assert.strictEqual(packageObj.version, '0.0.1')
+      assert.strictEqual(packageObj.description, "It's an awesome package man!")
+      assert.strictEqual(packageObj.main, 'index.js')
+      assert.strictEqual(packageObj.scripts.test, 'npm test')
+      assert.strictEqual(packageObj.keywords[0], 'awesome')
+      assert.strictEqual(packageObj.keywords[1], 'cool')
+      assert.strictEqual(packageObj.author, 'JP Richardson')
+      assert.strictEqual(packageObj.license, 'MIT')
+
+      done()
+    })
   })
 
-  describe('process', function() {
-    it('should automate the command: npm init', function(done) {
-      process.chdir(TEST_DIR);
-      suppose('npm', ['init'])
-        .on(/name\: \([\w|\-]+\)[\s]*/).respond("awesome_package\n")
-        //.on('name: (suppose-test) ').respond('awesome_package\n')
-        .on('version: (0.0.0) ').respond('0.0.1\n')
-        .on('description: ').respond("It's an awesome package man!\n")
-        .on('entry point: (index.js) ').respond("\n")
-        .on('test command: ').respond('npm test\n')
-        .on('git repository: ').respond("\n")
-        .on('keywords: ').respond('awesome, cool\n')
-        .on('author: ').respond('JP Richardson\n')
-        .on('license: (BSD) ').respond('MIT\n')
-        .on('ok? (yes) ' ).respond('yes\n')
-      .error(function (err) {
-        done(err)
-      })
-      .end(function(code){
-        EQ (code, 0)
-        var packageFile = path.join(TEST_DIR, 'package.json');
-        fs.readFile(packageFile, function(err, data){
-          var packageObj = JSON.parse(data.toString());
-          EQ (packageObj.name, 'awesome_package')
-          EQ (packageObj.version, '0.0.1')
-          EQ (packageObj.description, "It's an awesome package man!")
-          EQ (packageObj.main, 'index.js')
-          EQ (packageObj.scripts.test, 'npm test')
-          EQ (packageObj.keywords[0], 'awesome')
-          EQ (packageObj.keywords[1], 'cool')
-          EQ (packageObj.author, 'JP Richardson')
-          EQ (packageObj.license, 'MIT')
-          done();
-        })
-      })
+  it('should automate the command: npm init and output stdio', function(done)
+  {
+    var self = this
+
+    var debugFile = join(this.TEST_DIR, 'debug.txt')
+    process.chdir(this.TEST_DIR)
+
+    suppose('npm', ['init'], {debug: fs.createWriteStream(debugFile)})
+      .when(/name\: \([\w|\-]+\)[\s]*/).respond("awesome_package\n")
+      //.when('name: (test-suppose) ').respond('awesome_package\n')
+      .when('version: (1.0.0) ').respond('0.0.1\n')
+      .when('description: ').respond("It's an awesome package man!\n")
+      .when('entry point: (index.js) ').respond("\n")
+      .when('test command: ').respond('npm test\n')
+      .when('git repository: ').respond("\n")
+      .when('keywords: ').respond('awesome, cool\n')
+      .when('author: ').respond('JP Richardson\n')
+      .when('license: (ISC) ').respond('MIT\n')
+      .when('ok? (yes) ' ).respond('yes\n')
+    .end(function(code, signal)
+    {
+      assert.strictEqual(code, 0)
+
+      var packageObj = require(join(self.TEST_DIR, 'package.json'))
+
+      assert.strictEqual(packageObj.name, 'awesome_package')
+      assert.strictEqual(packageObj.version, '0.0.1')
+      assert.strictEqual(packageObj.description, "It's an awesome package man!")
+      assert.strictEqual(packageObj.main, 'index.js')
+      assert.strictEqual(packageObj.scripts.test, 'npm test')
+      assert.strictEqual(packageObj.keywords[0], 'awesome')
+      assert.strictEqual(packageObj.keywords[1], 'cool')
+      assert.strictEqual(packageObj.author, 'JP Richardson')
+      assert.strictEqual(packageObj.license, 'MIT')
+
+      var debugResFile = ''
+      if (os.platform() === 'darwin')
+        debugResFile = P('test/resources/debug-darwin.txt')
+      else
+        debugResFile = P('test/resources/debug-linux.txt')
+
+      //check debug file
+      var debugString = fs.readFileSync(debugFile).toString()
+      var checkString = fs.readFileSync(debugResFile).toString()
+
+      assert.strictEqual(debugString, checkString)
+
+      done()
     })
+  })
 
-    it('should automate the command: npm init and output stdio', function(done) {
-      var debugFile = path.join(TEST_DIR, 'debug.txt');
-      process.chdir(TEST_DIR);
-      suppose('npm', ['init'])
-        .debug(fs.createWriteStream(debugFile))
-        .on(/name\: \([\w|\-]+\)[\s]*/).respond("awesome_package\n")
-        //.on('name: (suppose-test) ').respond('awesome_package\n')
-        .on('version: (0.0.0) ').respond('0.0.1\n')
-        .on('description: ').respond("It's an awesome package man!\n")
-        .on('entry point: (index.js) ').respond("\n")
-        .on('test command: ').respond('npm test\n')
-        .on('git repository: ').respond("\n")
-        .on('keywords: ').respond('awesome, cool\n')
-        .on('author: ').respond('JP Richardson\n')
-        .on('license: (BSD) ').respond('MIT\n')
-        .on('ok? (yes) ' ).respond('yes\n')
-      .end(function(code){
-        EQ (code, 0);
-        var packageFile = path.join(TEST_DIR, 'package.json');
-        fs.readFile(packageFile, function(err, data){
-          var packageObj = JSON.parse(data.toString());
-          EQ (packageObj.name, 'awesome_package')
-          EQ (packageObj.version, '0.0.1')
-          EQ (packageObj.description, "It's an awesome package man!")
-          EQ (packageObj.main, 'index.js')
-          EQ (packageObj.scripts.test, 'npm test')
-          EQ (packageObj.keywords[0], 'awesome')
-          EQ (packageObj.keywords[1], 'cool')
-          EQ (packageObj.author, 'JP Richardson')
-          EQ (packageObj.license, 'MIT')
+  it('should emit the error event if there is an error', function(done)
+  {
+    process.chdir(this.TEST_DIR)
 
-          var debugResFile = ''
-          if (os.platform() === 'darwin')
-            debugResFile = P('test/resources/debug-darwin.txt')
-          else
-            debugResFile = P('test/resources/debug-linux.txt')
-
-          //check debug file
-          var debugString = fs.readFileSync(debugFile).toString();
-          var checkString = fs.readFileSync(debugResFile).toString();
-          EQ (debugString, checkString)
-          done()
-        })
-      })
+    suppose('node', ['this_script_does_not_exist.js'])
+    .on('error',function(err)
+    {
+      assert.notStrictEqual(err, undefined)
+      assert.ok(S(err.message).contains('Error: Cannot find module'))
     })
+    .end(function(code)
+    {
+      assert.notStrictEqual(code, 0)
 
-    it('should call the error method if there is an error', function(done) {
-      var someErr = null
-
-      process.chdir(TEST_DIR);
-      suppose('node', ['this_script_does_not_exist.js'])
-      .error(function(err) {
-        someErr = err
-      })
-      .end(function(code) {
-        T (code !== 0)
-        T (someErr);
-        T (S(someErr.message).startsWith('Error: Cannot find module'))
-        done()
-      })
+      done()
     })
+  })
 
-    it('should call a method as response', function(done) {
-      var pid = null
+  it('should call a method as response', function(done)
+  {
+    process.chdir(this.TEST_DIR)
 
-      process.chdir(TEST_DIR);
-      suppose('ls', ['-l'])
-      .on(/total \d+\s*/).respond(function(exe)
-      {
-        pid = exe.pid
-      })
-      .end(function(code) {
-        EQ (code, 0);
-        T (pid);
-        done()
-      })
+    suppose('ls', ['-l'])
+    .when(/total \d+\s*/).respond(function(exe)
+    {
+      assert.notStrictEqual(exe, undefined)
+      assert.notStrictEqual(exe.pid, undefined)
+    })
+    .end(function(code)
+    {
+      assert.strictEqual(code, 0)
+
+      done()
     })
   })
 })
